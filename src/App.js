d8a34dec86e37154488f41b058919d6beae9c3d1
Jay <jaydeepkachare123@gmail.com>
2022-04-01 22:11:45 +0530

    NavLink added for header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Product from "./pages/Products";
+import MainHeader from "./Components/MainHeader";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+
+        <Route path="/products">
+          <Product />
+        </Route>
+      </main>
     </div>
   );
 }
